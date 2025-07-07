c2102c3dfdbf89ba81ee51f2e2f5c3fcdc8fcd09
MQ255 <mahmoodqais20000@gmail.com>
2025-07-07 11:35:33 +0300

    Update ai_signal.py

--- ai_signal.py
+++ ai_signal.py
@@ -1,16 +1,4 @@
 import random
 
 def generate_ai_signal():
-    symbol = "BTCUSD"
-    direction = random.choice(["BUY", "SELL"])
-    volume = 0.03  # حجم الصفقة لكل 100$
-    tp_pips = 40
-    sl_pips = 20
-
-    return {
-        "symbol": symbol,
-        "direction": direction,
-        "volume": volume,
-        "tp_pips": tp_pips,
-        "sl_pips": sl_pips
-    }
+    return random.choice(["BUY", "SELL"])
