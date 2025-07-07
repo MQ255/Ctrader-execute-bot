4d44eebb17da2888286965f847f3e18dd13dacc6
MQ255 <mahmoodqais20000@gmail.com>
2025-07-07 11:10:44 +0300

    Update execute_trader.py

--- execute_trader.py
+++ execute_trader.py
@@ -1,25 +1,22 @@
-# execute_trader.py
-
 import requests
 
 ACCESS_TOKEN = "aeb_Sz7NOg_dGtjwV9hfEf2jazhk10kGPKyApuXmE5w"
 ACCOUNT_ID = "5217824"
 
 def place_order(symbol, direction, volume, tp_pips, sl_pips):
-    url = f"https://live.ctraderapi.com/v1/accounts/{ACCOUNT_ID}/orders/market"
-    
-    data = {
-        "symbol": symbol,
-        "volume": int(volume * 100000),
-        "side": "BUY" if direction == "BUY" else "SELL",
-        "takeProfitInPips": tp_pips,
-        "stopLossInPips": sl_pips
-    }
-
+    url = "https://api.spotware.com/connect/trading/accounts/{}/orders/market".format(ACCOUNT_ID)
     headers = {
         "Authorization": f"Bearer {ACCESS_TOKEN}",
         "Content-Type": "application/json"
     }
 
-    response = requests.post(url, json=data, headers=headers)
-    print("Order response:", response.text)
+    data = {
+        "symbol": symbol,
+        "direction": direction,
+        "volume": volume,
+        "tp_pips": tp_pips,
+        "sl_pips": sl_pips
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print("Order Response:", response.text)
